ee5b7afebc9bc41b6f12f39f07e0d79ba47eba77
Mossab Deh <mossabdehane19@gmail.com>
2023-10-09 16:38:54 +0200

    first commit

---
+++ src/main/java/dev/deh/rys/view/user/UserListView.java
@@ -0,0 +1,14 @@
+package dev.deh.rys.view.user;
+
+import dev.deh.rys.entity.User;
+import dev.deh.rys.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "users", layout = MainView.class)
+@ViewController("rys_User.list")
+@ViewDescriptor("user-list-view.xml")
+@LookupComponent("usersDataGrid")
+@DialogMode(width = "64em")
+public class UserListView extends StandardListView<User> {
+}
