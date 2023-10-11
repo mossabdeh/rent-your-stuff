2f9a1f9a5e9c4d68bcfa1f5538cde24008f6b21f
Mossab Deh <mossabdehane19@gmail.com>
2023-10-11 02:06:46 +0200

    create application , db and GH integration for heroku using TR

--- infrastructure/heroku.tf
+++ infrastructure/heroku.tf
@@ -10,3 +10,40 @@ resource "heroku_app" "rent-your-stuff-emerald" {
     "heroku/gradle"
   ]
 }
+
+# Create a database, and configure the app to use it
+resource "heroku_addon" "rent-your-stuff-emerald_db" {
+  app_id = heroku_app.rent-your-stuff-emerald.id
+  plan   = "heroku-postgresql:hobby-dev"
+}
+
+# Create a Pipeline
+resource "heroku_pipeline" "rent-your-stuff-pipeline" {
+  name = "rent-your-stuff-pipeline"
+}
+
+# Coupling app to pipeline
+resource "heroku_pipeline_coupling" "staging_pipeline_coupling" {
+  app_id   = heroku_app.rent-your-stuff-emerald.id
+  pipeline = heroku_pipeline.rent-your-stuff-pipeline.id
+  stage    = "Staging"
+}
+
+// Add the GitHub repository integration with the pipeline.
+resource "herokux_pipeline_github_integration" "foobar" {
+  pipeline_id = heroku_pipeline.rent-your-stuff-pipeline.id
+  org_repo = "mossabdeh/rent-your-stuff"
+}
+
+// Add Heroku app GitHub integration.
+resource "herokux_app_github_integration" "foobar" {
+  app_id = heroku_app.rent-your-stuff-emerald.uuid
+  branch = "main"
+  auto_deploy = true
+  wait_for_ci = true
+
+  # Tells Terraform that this resource must be created/updated
+  # only after the `herokux_pipeline_github_integration` has been successfully applied.
+  depends_on = [herokux_pipeline_github_integration.foobar]
+}
+
